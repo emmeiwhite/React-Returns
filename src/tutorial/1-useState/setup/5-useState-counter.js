8c756bc23f3fabd30d9be149a9dbf8aef49c5d10
Imran Rather <emmeiwhite@gmail.com>
2021-11-25 23:23:51 +0530

    Beautifuil Example covered

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -7,8 +7,9 @@ const UseStateCounter = () => {
     setCount(0);
   };
 
-  // Here we want to make multiple button clicks and expect them to increase the counter after certain time (say 3 seconds)
+  /*   Here we want to make multiple button clicks and expect them to increase the counter after certain time (say 3 seconds)*/
 
+  /* --
   const handleIncrease = () => {
     setTimeout(() => {
       setCount(count + 1); // This wouldn't work
@@ -20,9 +21,26 @@ const UseStateCounter = () => {
       setCount(count - 1); // This would also not work
     }, 3000);
   };
+  --*/
 
-  // Both the above function would not work as expected, because our state value is not updated while 3 seconds passed, it stayed the old one
-  // Solution is to update the state using function as argument to setCount(f)
+  /* Both the above function would not work as expected, because our state value is not updated while 3 seconds passed, it stayed the old one
+  Solution is to update the state using function as argument to setCount(f) */
+
+  const handleIncrease = () => {
+    setTimeout(() => {
+      setCount((prevCount) => {
+        return prevCount + 1;
+      }); // This wouldn't work
+    }, 3000);
+  };
+
+  const handleDecrease = () => {
+    setTimeout(() => {
+      setCount((prevCount) => {
+        return prevCount - 1;
+      }); // This would also not work
+    }, 3000);
+  };
 
   return (
     <>
