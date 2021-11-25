f2c6830d670e0616cdb986de3c7c8e6ace526af0
Imran Rather <emmeiwhite@gmail.com>
2021-11-25 19:26:19 +0530

    No re-render, when we try to update state directly

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -2,10 +2,19 @@ import React from "react";
 
 const ErrorExample = () => {
   let title = "Random Title";
+
+  const handleClick = (e) => {
+    title = "Title Updated";
+    console.log(title);
+  };
+  // Question is When we click on the button, would the title be updated?
+  // Answer is No. Because no re-rendering happened. App is in old state.
+  // Only when the re-render happens, if state has changed, then we can see the updates
+
   return (
     <>
       <h2>{title}</h2>
-      <button type="button" className="btn">
+      <button type="button" className="btn" onClick={handleClick}>
         Change Title
       </button>
     </>
