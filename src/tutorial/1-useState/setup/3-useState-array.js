1dc9cf92cacaa8d3894b4263e9e10e6f7e5fbb26
Imran Rather <emmeiwhite@gmail.com>
2021-11-25 22:15:51 +0530

    Completed delete items functionality

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -4,18 +4,26 @@ import { data } from "../../../data";
 const UseStateArray = () => {
   const [people, setPeople] = React.useState(data);
 
+  const handleRemove = (id) => {
+    const updatedPeople = people.filter((person) => person.id !== id);
+    setPeople(updatedPeople);
+  };
+
   return (
     <>
       {people.map(({ id, name }) => {
         return (
           <div className="item" key={id}>
             <h4>{name}</h4>
-            <button>remove</button>
+            <button onClick={() => handleRemove(id)}>remove</button>
           </div>
         );
       })}
 
-      <button className="btn">clear items</button>
+      {/* deleting all the items on button click */}
+      <button className="btn" onClick={() => setPeople([])}>
+        clear items
+      </button>
     </>
   );
 };
