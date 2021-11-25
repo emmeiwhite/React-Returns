e450094c3da619481bc1f6946281adb522eab2cd
Imran Rather <emmeiwhite@gmail.com>
2021-11-25 22:31:43 +0530

    Counter App working fine.

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <h2>Regular Counter</h2>
+      <h1>{count}</h1>
+
+      <button className="btn" onClick={() => setCount(count + 1)}>
+        increase
+      </button>
+      <button className="btn" onClick={() => setCount(0)}>
+        reset
+      </button>
+      <button className="btn" onClick={() => setCount(count - 1)}>
+        decrease
+      </button>
+    </>
+  );
 };
 
 export default UseStateCounter;
