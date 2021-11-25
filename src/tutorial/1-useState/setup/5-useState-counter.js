4ae04fe4737b88ce50c57e659a98577a7c14a3c6
Imran Rather <emmeiwhite@gmail.com>
2021-11-25 22:33:04 +0530

    Created internal event handler for reset

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 
 const UseStateCounter = () => {
   const [count, setCount] = useState(0);
+
+  const handleReset = () => {
+    setCount(0);
+  };
   return (
     <>
       <h2>Regular Counter</h2>
@@ -10,7 +14,7 @@ const UseStateCounter = () => {
       <button className="btn" onClick={() => setCount(count + 1)}>
         increase
       </button>
-      <button className="btn" onClick={() => setCount(0)}>
+      <button className="btn" onClick={handleReset}>
         reset
       </button>
       <button className="btn" onClick={() => setCount(count - 1)}>
