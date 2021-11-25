1bfe4c9f7b5fcce1f57b9b7b7ae559a381986f68
Imran Rather <emmeiwhite@gmail.com>
2021-11-25 23:19:31 +0530

    More Complex Counter example (In progress)

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -6,6 +6,24 @@ const UseStateCounter = () => {
   const handleReset = () => {
     setCount(0);
   };
+
+  // Here we want to make multiple button clicks and expect them to increase the counter after certain time (say 3 seconds)
+
+  const handleIncrease = () => {
+    setTimeout(() => {
+      setCount(count + 1); // This wouldn't work
+    }, 3000);
+  };
+
+  const handleDecrease = () => {
+    setTimeout(() => {
+      setCount(count - 1); // This would also not work
+    }, 3000);
+  };
+
+  // Both the above function would not work as expected, because our state value is not updated while 3 seconds passed, it stayed the old one
+  // Solution is to update the state using function as argument to setCount(f)
+
   return (
     <>
       <h2>Regular Counter</h2>
@@ -18,6 +36,19 @@ const UseStateCounter = () => {
         reset
       </button>
       <button className="btn" onClick={() => setCount(count - 1)}>
+        decrease
+      </button>
+
+      <h2 style={{ margin: "4rem 0" }}>More Complex Counter</h2>
+      <h1>{count}</h1>
+
+      <button className="btn" onClick={handleIncrease}>
+        increase
+      </button>
+      <button className="btn" onClick={handleReset}>
+        reset
+      </button>
+      <button className="btn" onClick={handleDecrease}>
         decrease
       </button>
     </>
