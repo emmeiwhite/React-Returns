52d46925041c502f6856cfcae3b3a06a2f223047
Imran Rather <emmeiwhite@gmail.com>
2021-11-26 14:03:31 +0530

    useEffect Hook for site effects

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+// useEffect is used for all Side Effects. console.log, API calls etc etc
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  // By Default useEffect runs after every re-render and performs site effects
+  useEffect(() => {
+    console.log("useEffect Hook Invoked !!!");
+    document.title = `updated title ${value}`; // performing site effect
+  });
+  return (
+    <main>
+      <h2>{value}</h2>
+      <button className="btn" onClick={() => setValue(value + 1)}>
+        Increase Value
+      </button>
+    </main>
+  );
 };
 
 export default UseEffectBasics;
