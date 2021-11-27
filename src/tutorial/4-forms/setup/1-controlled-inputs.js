f5f3c1ed53d8280fc341e803f0f19a39098700f4
Imran Rather <emmeiwhite@gmail.com>
2021-11-27 18:44:50 +0530

    Adding items to list

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -8,11 +8,24 @@ import React, { useState } from "react";
 const ControlledInputs = () => {
   const [userName, setUserName] = useState("");
   const [userEmail, setUserEmail] = useState("");
+
+  // Adding items to the list
+
+  const [people, setPeople] = useState([]);
   const handleSubmit = (e) => {
     e.preventDefault();
 
     if (userName && userEmail) {
       console.log("Form Submit");
+      const person = {
+        id: new Date().getTime().toString(),
+        userName,
+        userEmail,
+      };
+
+      setPeople((people) => {
+        return [...people, person];
+      });
       setUserName("");
       setUserEmail("");
     } else {
@@ -21,31 +34,45 @@ const ControlledInputs = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="form">
-      <div className="form-control">
-        <label htmlFor="userName">Name :</label>
-        <input
-          type="text"
-          name="userName"
-          onChange={(e) => setUserName(e.target.value)}
-          value={userName}
-          id="userName"
-        />
-      </div>
+    <>
+      <form onSubmit={handleSubmit} className="form">
+        <div className="form-control">
+          <label htmlFor="userName">Name :</label>
+          <input
+            type="text"
+            name="userName"
+            onChange={(e) => setUserName(e.target.value)}
+            value={userName}
+            id="userName"
+          />
+        </div>
 
-      <div className="form-control">
-        <label htmlFor="userEmail">Email :</label>
-        <input
-          type="text"
-          name="userEmail"
-          onChange={(e) => setUserEmail(e.target.value)}
-          value={userEmail}
-          id="userEmail"
-        />
-      </div>
+        <div className="form-control">
+          <label htmlFor="userEmail">Email :</label>
+          <input
+            type="text"
+            name="userEmail"
+            onChange={(e) => setUserEmail(e.target.value)}
+            value={userEmail}
+            id="userEmail"
+          />
+        </div>
 
-      <button type="submit">Submit</button>
-    </form>
+        <button type="submit">Submit</button>
+      </form>
+
+      {people.length > 0 && <h2>People :</h2>}
+      {people.length > 0 &&
+        people.map((person) => {
+          const { id, userName, userEmail } = person;
+          return (
+            <div className="item" key={id}>
+              <h4>{userName}</h4>
+              <p>{userEmail}</p>
+            </div>
+          );
+        })}
+    </>
   );
 };
 
