b7dc1a62f0841ec04990a1d3a44bf6f8ec43f5fd
Imran Rather <emmeiwhite@gmail.com>
2021-11-27 18:18:08 +0530

    Form Submission in React.

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // JS
 // const input = document.getElementById('myText');
 // const inputValue = input.value
@@ -6,7 +6,26 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form Submit");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="form">
+      <div class="form-control">
+        <label htmlFor="userName">Name :</label>
+        <input type="text" name="userName" id="userName" />
+      </div>
+
+      <div class="form-control">
+        <label htmlFor="userEmail">Email :</label>
+        <input type="text" name="userEmail" id="userEmail" />
+      </div>
+
+      <button type="submit">Submit</button>
+    </form>
+  );
 };
 
 export default ControlledInputs;
