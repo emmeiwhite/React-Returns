cdcacf76d05c18f92cc556ce96ba9c3cac4bdf9a
Imran Rather <emmeiwhite@gmail.com>
2021-11-27 07:25:20 +0530

    short-circuit evaluation - || &&

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
@@ -6,7 +6,16 @@ const ShortCircuit = () => {
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  return (
+    <main>
+      {/*
+       {if(true){console.log("Hello World")}} 
+       This expression is not allowed within JSX.
+       Because it doesno't return any value. Instead 
+       we make use of the short-circuit operator and ternairy operators when required
+       */}
+    </main>
+  );
 };
 
 export default ShortCircuit;
